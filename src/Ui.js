915b2965a8c84b7817401c36eaf6b74916e1c6e8
asada0701 <yusuke.nakashima@topgate.co.jp>
2017-06-28 12:35:15 +0900

    uiクラスの解体

--- src/Ui.js
+++ src/Ui.js
@@ -1,137 +1,21 @@
-import { CpuLevel } from './cpuLevel.js';
-
 const State = Object.freeze({ 0: '_', 1: '○', 2: '×' });
 
-/**
- * UIクラス
- * TODO gameMatch.boardをthis.boardとかにした方がわかりやすいかもしれない。
- * @author asada
- */
-export default class Ui {
-    /**
-     * コンストラクタ
-     * タイトル、CPUのセレクトボックス、ゲームボード、リセットボタンを作成し、表示する。
-     */
-    constructor() {
-        const createTitle = () => {
-            const title = document.createElement('h1');
-            title.innerHTML = '○×ゲーム';
-            return title;
-        };
-
-        const createCpuLevelSelectBox = () => {
-            const pTag = document.createElement('p');
-            pTag.innerHTML = 'CPUの難易度:';
-
-            //セレクトボックスを作る
-            const select = document.createElement('select');
-            select.id = 'CpuLevel';
-            select.addEventListener('change', () => {
-                gameMatch.board.init();
-                gameMatch.ui.printBoard();
-                setCpu(document.getElementById('CpuLevel').value);
-            });
-
-            //オプションを作る
-            for (let value of Object.keys(CpuLevel)) {
-                let option = document.createElement('option');
-                option.value = CpuLevel[value];
-                option.innerHTML = CpuLevel[value];
-                select.appendChild(option);
-            }
-
-            pTag.appendChild(select);
-            return pTag;
-        };
-
-        /**
-         * CPUの強さを変更する
-         *
-         * @param cpuLevel
-         */
-        const setCpu = function (cpuLevel) {
-            switch (cpuLevel) {
-                case CpuLevel.EASY:
-                    cpu = new EasyCpu(2);
-                    break;
-
-                default:
-                    window.alert('存在しないCPUが選択されました。');
-            }
-        }
-
-        const createGameBoard = () => {
-            const fragment = document.createDocumentFragment();
-
-            //pタグで段落をつける
-            let pTag = document.createElement('p');
-            for (let i = 0; i < gameMatch.board.oneSideLength * gameMatch.board.oneSideLength; i++) {
-                if (i % gameMatch.board.oneSideLength === 0) {
-                    pTag = document.createElement('p');
-                }
-
-                let button = document.createElement('button');
-                //TODO ここでIDを消すと、'innerHTML' of nul　となる原因について調べる。
-                button.id = `${i}`;
-                button.innerHTML = State[gameMatch.board.GAME_BOARD_DEFAULT_VALUE];
-                button.addEventListener('click', () => {
-                    humanPlayer.selectByUser(Math.floor(i / gameMatch.board.oneSideLength), i % gameMatch.board.oneSideLength);
-                });
-
-                pTag.appendChild(button);
-                fragment.appendChild(pTag);
-            }
-            return fragment;
-        };
-
-        const createResetButton = () => {
-            const resetButton = document.createElement('button');
-            resetButton.innerHTML = 'リセット';
-            resetButton.addEventListener('click', () => {
-                gameMatch.board.init();
-                gameMatch.ui.printBoard();
-            });
-            return resetButton;
-        };
-
-        const createDOM = () => {
-            const divClassContent = document.createElement('div');
-            divClassContent.className = 'content';
-
-            divClassContent.appendChild(createTitle());
-
-            divClassContent.appendChild(createCpuLevelSelectBox());
-
-            divClassContent.appendChild(createGameBoard());
-
-            divClassContent.appendChild(createResetButton());
-
-            return divClassContent;
-        };
-
-        const el = createDOM();
-        document.getElementById('root').appendChild(el);
-    }
-
+export default Ui = {
     /**
      * 現在のボードの状況を表示する。
      */
-    printBoard() {
-        for (let i = 0; i < gameMatch.board.oneSideLength * gameMatch.board.oneSideLength; i++) {
-            document.getElementById(`${i}`).innerHTML = State[gameMatch.board.gameBoardArray[Math.floor(i / gameMatch.board.oneSideLength)][i % gameMatch.board.oneSideLength]];
+    printBoard: function (board) {
+        for (let i = 0; i < board.oneSideLength * board.oneSideLength; i++) {
+            document.getElementById(`${i}`).innerHTML = State[board.gameBoardArray[Math.floor(i / board.oneSideLength)][i % board.oneSideLength]];
         }
-    }
-
+    },
     /**
-     * 結果を表示する
-     *
-     * @param result Resultオブジェクトの項目を渡すこと。
+     * 結果を表示する。
      */
-    printResultMessage(result) {
-        this.printBoard();
+    printResultMessage(board, result) {
+        this.printBoard(board);
         window.alert(result);
-    }
-
+    },
     printIsAlreadyPutMessage() {
         window.alert('そこはすでに埋まっています。');
     }
--- src/app.js
+++ src/app.js
@@ -10,8 +10,11 @@ import { EasyCpu } from './cpu.js';
  */
 class Game3by3HumanVsCpu {
     constructor() {
-        this.ui = new Ui();
-        this.board = new SquareBoard(this.ui, 3, 3);
+        const x = 3;
+        const y = 3;
+
+        this.ui = new Ui(x, y);
+        this.board = new SquareBoard(x, y);
         this.humanPlayer = new HumanPlayer(1);
         this.cpu = new EasyCpu(2);
     }
@@ -34,3 +37,102 @@ class Game3by3HumanVsCpu {
 }
 
 let gameMatch = new Game3by3HumanVsCpu(board, ui, humanPlayer, cpu);
+
+const el = createDOM();
+document.getElementById('root').appendChild(el);
+
+function createDOM() {
+    const divClassContent = document.createElement('div');
+    divClassContent.className = 'content';
+
+    divClassContent.appendChild(createTitle());
+
+    divClassContent.appendChild(createCpuLevelSelectBox());
+
+    divClassContent.appendChild(createGameBoard());
+
+    divClassContent.appendChild(createResetButton());
+
+    return divClassContent;
+}
+
+function createTitle() {
+    const title = document.createElement('h1');
+    title.innerHTML = '○×ゲーム';
+    return title;
+}
+
+function createCpuLevelSelectBox() {
+    const pTag = document.createElement('p');
+    pTag.innerHTML = 'CPUの難易度:';
+
+    //セレクトボックスを作る
+    const select = document.createElement('select');
+    select.id = 'CpuLevel';
+    select.addEventListener('change', () => {
+        gameMatch.board.init();
+        gameMatch.ui.printBoard();
+        setCpu(document.getElementById('CpuLevel').value);
+    });
+
+    //オプションを作る
+    for (let value of Object.keys(CpuLevel)) {
+        let option = document.createElement('option');
+        option.value = CpuLevel[value];
+        option.innerHTML = CpuLevel[value];
+        select.appendChild(option);
+    }
+
+    pTag.appendChild(select);
+    return pTag;
+}
+
+/**
+ * CPUの強さを変更する
+ *
+ * @param cpuLevel
+ */
+function setCpu(cpuLevel) {
+    switch (cpuLevel) {
+        case CpuLevel.EASY:
+            cpu = new EasyCpu(2);
+            break;
+
+        default:
+            window.alert('存在しないCPUが選択されました。');
+    }
+}
+
+function createGameBoard() {
+    const fragment = document.createDocumentFragment();
+
+    //pタグで段落をつける
+    let pTag = document.createElement('p');
+    for (let i = 0; i < gameMatch.board.oneSideLength * gameMatch.board.oneSideLength; i++) {
+        if (i % gameMatch.board.oneSideLength === 0) {
+            pTag = document.createElement('p');
+        }
+
+        let button = document.createElement('button');
+        //TODO ここでIDを消すと、'innerHTML' of nul　となる原因について調べる。
+        button.id = `${i}`;
+        button.innerHTML = State[gameMatch.board.GAME_BOARD_DEFAULT_VALUE];
+        button.addEventListener('click', () => {
+            humanPlayer.selectByUser(Math.floor(i / gameMatch.board.oneSideLength), i % gameMatch.board.oneSideLength);
+        });
+
+        pTag.appendChild(button);
+        fragment.appendChild(pTag);
+    }
+    return fragment;
+}
+
+function createResetButton() {
+    const resetButton = document.createElement('button');
+    resetButton.innerHTML = 'リセット';
+    resetButton.addEventListener('click', () => {
+        gameMatch.board.init();
+        gameMatch.ui.printBoard();
+    });
+    return resetButton;
+}
